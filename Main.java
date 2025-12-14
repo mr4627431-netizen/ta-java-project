2d01f6f14fa7de104f620f161ae3360d39d90e8e
Mahdi rezaei <mr4627431@gmail.com>
2025-12-15 00:24:28 +0330

    Add files via upload

--- Main.java
+++ Main.java
@@ -6,14 +6,17 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("enter number :");
-        int n = scanner.nextInt();
+        int n=scanner.nextInt();
         int s=0;
         int p=1;
         while(n>0) {
-            s = s  + (n % 2)*p;
-            n = n / 2;
-            p = p*10;
+            if (n % 10> 0) {
+                s = s + ((n % 10) * p);
+                p = p * 10;
+            }
+            n = n / 10;
         }
-        System.out.println(s);
+    System.out.println(s);
+
     }
 }
